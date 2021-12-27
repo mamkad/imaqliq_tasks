50013a5ebec32071fc8210785aca4091ca603e5c
mamkad <89218729+mamkad@users.noreply.github.com>
2021-12-27 20:41:48 +0300

    Update filerw.c

--- file/filerw.c
+++ file/filerw.c
@@ -2,8 +2,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int save_file(char const* file_name, char const* buff, size_t size)
+#define FILELENGTHSIZE 17
+
+int save_file(char const* file_name, char const* buff, long int size)
 {
 	FILE* file = fopen(file_name, "wb");
 	if (file == NULL) {
@@ -16,33 +19,47 @@ int save_file(char const* file_name, char const* buff, size_t size)
 	return nbytes;
 }
 
-int open_file(char const* file_name, char* buff, size_t size)
+char* open_file(char const* file_name, long int* size)
 {
 	FILE * file = fopen(file_name , "rb");
 	if (file == NULL) {
-	 	return -1;
+	 	return NULL;
 	}
 	
     int ret_code = fseek(file , 0 , SEEK_END);   
     if (ret_code != 0) {
     	fclose (file);
-    	return -1;
+    	return NULL;
     }
      
-    size_t nsize = ftell(file);  
-    if (nsize == -1L) {
+    *size = ftell(file);  
+    if (*size == -1L) {
     	fclose (file);
-    	return -1;
+    	return NULL;
     }
      
     ret_code = fseek(file , 0 , SEEK_SET); 
     if (ret_code != 0) {
     	fclose (file);
-    	return -1;
-    }                 
- 	
-  	size_t nbytes = fread(buff, sizeof(char), size, file);       
+    	return NULL;
+    }
+    
+    char size_number[FILELENGTHSIZE];
+    size_number[0] = '\0';
+    
+    int cur_number_size = sprintf(size_number, "%ld", *size);
+    (*size) += FILELENGTHSIZE;
+    
+    char * buff = (char*)malloc(sizeof(char) * (*size));
+    if (buff == NULL) {
+    	fclose (file);
+    	return NULL;
+    }   
+    strncpy(buff, size_number, cur_number_size);
+    memset(buff + cur_number_size, ' ', FILELENGTHSIZE - cur_number_size - 1);
+    buff[FILELENGTHSIZE - 1] = '\n';
+
+  	size_t nbytes = fread(buff + FILELENGTHSIZE, sizeof(char), (*size), file);       
   	fclose (file);
- 
-  	return nbytes;
+  	return buff;
 }
