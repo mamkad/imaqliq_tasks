b3bd8d1389560fa6d63e4d900e03047ac0216e63
mamkad <89218729+mamkad@users.noreply.github.com>
2021-12-27 20:46:14 +0300

    Update filerw.h

--- file/filerw.h
+++ file/filerw.h
@@ -4,6 +4,5 @@
 #include <stddef.h>
 
 int save_file(char const* file_name, char const* buff, size_t size);
-int open_file(char const* file_name, char* buff, size_t size);
-
+char* open_file(char const* file_name, long int* size);
 #endif
